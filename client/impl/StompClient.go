a6d7be16d9b6459adeef00402ab3247d7e22aa15
Fabio Kaufmann <baselzockt@gmail.com>
2021-03-12 15:16:12 +0100

    added logging

--- client/impl/StompClient.go
+++ client/impl/StompClient.go
@@ -2,8 +2,8 @@ package impl
 
 import (
 	"errors"
-	"fmt"
 	"github.com/go-stomp/stomp"
+	log "github.com/sirupsen/logrus"
 )
 
 type StompClient struct {
@@ -16,32 +16,40 @@ func NewStompClient() *StompClient {
 }
 
 func (s *StompClient) Connect(url string) error {
+	log.Debug("Setting up subscription map")
 	s.subscriptions = map[string]*stomp.Subscription{}
 	var err error
+	log.Debug("trying to connect to: " + url)
 	s.conn, err = stomp.Dial("tcp", url)
+	if err == nil {
+		log.Debug("Successfully connected")
+	}
 	return err
 }
 
 func (s *StompClient) SubscribeToQueue(queueName string, messageChanel chan []byte) error {
+	log.Debug("Trying to subscribe to: " + queueName)
 	if s.conn != nil {
 		sub, err := s.conn.Subscribe(queueName, stomp.AckAuto)
 		if err == nil {
-			if s.subscriptions == nil {
-				s.subscriptions = map[string]*stomp.Subscription{}
-			}
+			log.Debug("Subscription was successful, adding it to list")
 			s.subscriptions[queueName] = sub
+			log.Debug("Starting go function to convert from a stomp specific channel to chan []byte ")
 			go func(subscription *stomp.Subscription, c chan []byte) {
 				for {
+					log.Debug("Trying to convert")
 					val := <-subscription.C
 					if val != nil {
 						c <- val.Body
 					} else {
-						fmt.Println("Subscription timed out, renewing...")
+						log.Debug("Subscription timed out, renewing...")
 						_ = s.SubscribeToQueue(queueName, c)
 						break
 					}
+					log.Debug("Conversion successful")
 				}
 			}(sub, messageChanel)
+			log.Debug("Function started")
 			return nil
 		}
 		return err
@@ -50,20 +58,25 @@ func (s *StompClient) SubscribeToQueue(queueName string, messageChanel chan []by
 }
 
 func (s *StompClient) Unsubscribe(queueName string) error {
+	log.Debug("Trying to unsubscribe from queue: " + queueName)
 	if s.subscriptions != nil {
+		log.Debug("Unsubscribing")
 		return s.subscriptions[queueName].Unsubscribe()
 	}
 	return errors.New("no subscriptions available")
 }
 
 func (s *StompClient) Disconnect() error {
+	log.Debug("Trying to disconnect")
 	if s.conn != nil {
 		return s.conn.Disconnect()
 	}
+	log.Error("Could not disconnect since there was no connection")
 	return errors.New("client was nil")
 }
 
 func (s *StompClient) SendMessageToQueue(queueName, contentType string, body []byte) error {
+	log.Debug("Trying to send message to Queue")
 	if s.conn != nil {
 		return s.conn.Send(queueName, contentType, body)
 	}
--- go.mod
+++ go.mod
@@ -1,7 +1,9 @@
 module github.com/Baselzockt/GoMQ
+
 go 1.16
 
 require (
 	github.com/go-stomp/stomp v2.1.2+incompatible
+	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )

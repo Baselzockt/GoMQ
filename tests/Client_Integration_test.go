ca177e1ba3f73111aa0df0eb640764e6d9bde50f
Fabio Kaufmann <baselzockt@gmail.com>
2021-03-12 14:16:01 +0100

    refactoring

---
+++ tests/Client_Integration_test.go
@@ -0,0 +1,30 @@
+package tests
+
+import (
+	"github.com/Baselzockt/GoMQ/client/impl"
+	"github.com/Baselzockt/GoMQ/content"
+	"testing"
+)
+
+func TestSendAndReceive(t *testing.T) {
+	client := impl.StompClient{}
+	err := client.Connect("localhost:61613")
+	checkForError(t, err)
+	channel := make(chan []byte)
+	err = client.SubscribeToQueue("test", channel)
+	checkForError(t, err)
+	want := "Integration test :D"
+	client.SendMessageToQueue("test", content.TEXT, []byte(want))
+	got := string(<-channel)
+	if got != want {
+		t.Errorf("Got %s want %s", got, want)
+	}
+	client.Disconnect()
+}
+
+func checkForError(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("Could not subscribe to queue because of error: %s", err.Error())
+	}
+}

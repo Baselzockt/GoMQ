2027df86c4f40e6faf6ed24436a3e7f1cbf4292a
Fabio Kaufmann <baselzockt@gmail.com>
2021-04-15 13:08:47 +0200

    added "connecting" behaviour to mock. Mock will now return errors when not connected on methods requiring connection

--- client/impl/MockClient.go
+++ client/impl/MockClient.go
@@ -1,32 +1,46 @@
 package impl
 
+import "errors"
+
 type MockClient struct {
 	calls         []string
 	messages      [][]byte
 	messageChanel map[string]chan []byte
+	connected     bool
 }
 
 func NewMockClient() *MockClient {
-	return &MockClient{[]string{}, [][]byte{}, map[string]chan []byte{}}
+	return &MockClient{[]string{}, [][]byte{}, map[string]chan []byte{}, false}
 }
 
 func (m *MockClient) Connect(url string) error {
 	m.calls = append(m.calls, "Connect to "+url)
+	m.connected = true
 	return nil
 }
 
 func (m *MockClient) Disconnect() error {
 	m.calls = append(m.calls, "Disconnect")
+	m.connected = false
 	return nil
 }
 
 func (m *MockClient) SubscribeToQueue(queueName string, messageChanel *chan []byte) error {
+
+	if !m.connected {
+		return errors.New("client not connected")
+	}
+
 	m.calls = append(m.calls, "Subscribe to "+queueName)
 	m.messageChanel[queueName] = *messageChanel
 	return nil
 }
 
 func (m *MockClient) Unsubscribe(queueName string) error {
+	if !m.connected {
+		return errors.New("client not connected")
+	}
+
 	m.calls = append(m.calls, "Unsubscribe from "+queueName)
 	close(m.messageChanel[queueName])
 	delete(m.messageChanel, queueName)
@@ -34,6 +48,10 @@ func (m *MockClient) Unsubscribe(queueName string) error {
 }
 
 func (m *MockClient) SendMessageToQueue(queueName, contentType string, body []byte) error {
+	if !m.connected {
+		return errors.New("client not connected")
+	}
+
 	m.calls = append(m.calls, "Sent message")
 	m.messages = append(m.messages, body)
 	go func(msg []byte) {
